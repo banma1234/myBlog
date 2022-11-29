8e6fac3f4b98de3ed451c05d9da39c65204ec66b
banma70 <banma1234@gmail.com>
2022-11-29 14:59:15 +0900

    build: default

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    // 사용자지정 옵션
+    createDefaultProgram: true,
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    // 'React' must be in scope when using JSX 에러 지우기(Next.js)
+    "react/react-in-jsx-scope": "off",
+    // ts파일에서 tsx구문 허용(Next.js)
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }], // should add ".ts" if typescript project
+  },
+};
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+const Hello = () => {
+  const greeting = "hello";
+  console.log(greeting);
+};
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default Hello;
