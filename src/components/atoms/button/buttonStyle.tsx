f940f25498268c9fe48c254a88029b9fee1cb7ad
banma1234 <banma1234@gmail.com>
2023-01-11 22:11:05 +0900

    design($atoms/button): border-radius: 10px => 999999px, 불필요 구문 제거

--- src/components/atoms/button/buttonStyle.tsx
+++ src/components/atoms/button/buttonStyle.tsx
@@ -10,24 +10,17 @@ const StyledButton = styled.button.attrs(props => ({}))<ButtonType>`
     const typeHandler = props.type;
     return css`
       background-color: ${useColor(props.color)};
-      color: ${
-        props.color == "gray" || props.color == "og_white" ? "black" : "white"
-      };
+      color: ${props.color == "gray" || props.color == "og_white"
+        ? "black"
+        : "white"};
       width: ${width[typeHandler]};
       height: ${height[typeHandler]};
       border: none;
       padding: 5px 10px;
-      font-size: 1em;
-      font-weight: 500;
-      outline: none;
+      margin-top: 10px;
       cursor: pointer;
       transition: all 0.3s;
-      border-radius: 10px;
-      transition: {
-        transition-duration: 0.2s, 0.2s;
-        transition-delay: 0s, 0s;
-        transition-property: color, background-color: ${useColor("green")};
-      }
+      border-radius: 999999px;
     `;
   }};
 `;
