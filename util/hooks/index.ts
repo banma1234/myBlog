7eca391c162ed1b4fc8da5025dda97c0f2a5d773
banma1234 <banma1234@gmail.com>
2023-01-20 23:42:11 +0900

    feat($util/hooks/~): 날짜변환 custom hooks 작성

--- util/hooks/index.ts
+++ util/hooks/index.ts
@@ -1,9 +1,11 @@
 import useColor from "./useColor/useColor";
+import useDate from "./useDate/useDate";
 
 const Hooks = {
   useColor,
+  useDate,
 };
 
 export default Hooks;
 
-export { useColor };
+export { useColor, useDate };
