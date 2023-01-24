a3c2f1e6c0922780ca374e845887573a42d47882
banma1234 <banma1234@gmail.com>
2023-01-24 18:57:14 +0900

    feat($pages/index.tsx): 메인화면에 게시글 뿌리는 기능 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import { getSortedPostsData } from 'util/posts';
+import Link from 'next/link';
+import { Layout } from "../src/components/organisms";
+import { Card } from '../src/components/molecules';
+
+export default function Home({ allPostsData }:any) {
   return (
-    <>
+    <Layout>
       <h1>Home</h1>
-    </>
+      <section>
+      {allPostsData && allPostsData.map(({id, title}:any) => (
+        <Link href={`/posts/${id}`}>
+          <Card type='default' color='pink'>
+            <h2>{title}</h2>
+          </Card>
+        </Link>
+      ))}
+      </section>
+    </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
