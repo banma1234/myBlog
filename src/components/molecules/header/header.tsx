76c377a05d39910de8bfc81f6774f33058344a8e
banma1234 <banma1234@gmail.com>
2023-01-30 21:45:48 +0900

    feat($molecules/header): component header 작성

--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -1,8 +1,30 @@
-import StyledHeader from "./headerStyles";
+import {
+  StyledHeader,
+  HeaderContainer,
+  HeaderIcon,
+  HeaderMenu,
+} from "./headerStyles";
 import { HeaderType } from "./headerType";
+import Link from "next/link";
 
 const HeaderComponent: React.FC<HeaderType> = (props: HeaderType) => {
-  return <StyledHeader {...props}>{props.children}</StyledHeader>;
+  return (
+    <StyledHeader {...props}>
+      <HeaderContainer>
+        <HeaderIcon>
+          <Link href="/">Home</Link>
+        </HeaderIcon>
+        <HeaderMenu>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/">temp</Link>
+          </li>
+        </HeaderMenu>
+      </HeaderContainer>
+    </StyledHeader>
+  );
 };
 
 export default HeaderComponent;
