9a7787c67595c3047f0e91527e678a0077e65ef7
banma1234 <banma1234@gmail.com>
2023-01-30 21:47:41 +0900

    fix($molecules/card/~): next/image tag 구버전 속성값 수정

--- src/components/molecules/card/card.tsx
+++ src/components/molecules/card/card.tsx
@@ -1,13 +1,13 @@
 import { StyledCard, ImageWrapper, Post } from "./cardStyle";
 import { CardType } from "./cardType";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import imgUrl from "public/testImg.jpg";
 
 const CardComponent: React.FC<CardType> = (props: CardType) => {
   return (
     <StyledCard {...props} color={props.color} type={props.type}>
       <ImageWrapper>
-        <Image src={imgUrl} alt="card Img" layout="responsive" />
+        <Image src={imgUrl} alt="card Img" priority />
       </ImageWrapper>
       <Post>{props.children}</Post>
     </StyledCard>
