758209bb26149f56ac0db330c58757a30365e6cf
banma1234 <banma1234@gmail.com>
2023-01-31 23:37:15 +0900

    feat($molecules/footer/~): component footer 작성

--- src/components/molecules/footer/footer.tsx
+++ src/components/molecules/footer/footer.tsx
@@ -1,8 +1,29 @@
-import StyledFooter from "./footerStyles";
-import { Footertype } from "./footerType";
+import { StyledFooter, FooterContainer, FooterMenu } from "./footerStyles";
+import { useIcons } from "util/hooks";
+import { ImgWrapper } from "styles/globals";
+import Link from "next/link";
+import Image from "next/legacy/image";
+import imgUrl from "public/testImg.jpg";
 
-const FooterComponent: React.FC<Footertype> = (props: Footertype) => {
-  return <StyledFooter {...props}>{props.children}</StyledFooter>;
+const FooterComponent: React.FC = () => {
+  return (
+    <StyledFooter>
+      <FooterContainer>
+        <ImgWrapper type="profile">
+          <Image src={imgUrl} alt="profile img" priority />
+        </ImgWrapper>
+        <FooterMenu>
+          <h1>Choco Ham</h1>
+          <li>{useIcons("mail", "16")} &nbsp; banma1234@gmail.com</li>
+          <li>
+            <Link href="https://github.com/banma1234">
+              {useIcons("github", "16")} &nbsp; github.com/banma1234
+            </Link>
+          </li>
+        </FooterMenu>
+      </FooterContainer>
+    </StyledFooter>
+  );
 };
 
 export default FooterComponent;
--- src/components/molecules/footer/footerStyles.tsx
+++ src/components/molecules/footer/footerStyles.tsx
@@ -1,10 +1,35 @@
 import styled, { css } from "styled-components";
 import { useColor } from "util/hooks";
 
-const StyledFooter = styled.div`
+export const StyledFooter = styled.div`
   width: 100%;
   height: 12rem;
   background-color: ${useColor("black")};
 `;
 
-export default StyledFooter;
+export const FooterContainer = styled.div`
+  display: grid;
+  height: 80%;
+  justify-content: center;
+  padding: 1rem;
+  border-left: 2rem solid;
+  border-color: ${useColor("green")};
+  color: ${useColor("gray")};
+  grid-template-columns: 160px 1fr;
+  @media all and (max-width: 576px) {
+    grid-template-columns: 0 1fr;
+  }
+`;
+
+export const FooterMenu = styled.ul`
+  list-style: none;
+  margin-top: -0.2rem;
+  margin-left: -1.5rem;
+  li {
+    padding: 0.3rem;
+    &:hover {
+      transition: all 0.3s;
+      color: ${useColor("green")};
+    }
+  }
+`;
