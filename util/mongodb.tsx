3a43fb0902f05dfab0a67a866a8d330875928d5a
banma1234 <banma1234@gmail.com>
2023-02-01 22:44:23 +0900

    feat($util/mongodb.tsx): 클라이언트와 mongoDB 연결하는 기능 작성

---
+++ util/mongodb.tsx
@@ -0,0 +1,48 @@
+import { MongoClient } from 'mongodb';
+
+const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_DB = process.env.DB_NAME;
+
+// check the MongoDB URL
+if (!MONGODB_URL) {
+    throw new Error('Define the MONGODB_URL environmental variable');
+}
+
+// check the MongoDB DB
+if (!MONGODB_DB) {
+    throw new Error('Define the MONGODB_DB environmental variable');
+}
+
+let cachedClient:any = null;
+let cachedDb:any = null;
+
+export async function connectToDatabase() {
+    // check the cached.
+    if (cachedClient && cachedDb) {
+        // load from cache
+        return {
+            client: cachedClient,
+            db: cachedDb,
+        };
+    }
+
+    // set the connection options
+    const opts:any = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
+    // Connect to cluster
+    let client = new MongoClient(MONGODB_URL!, opts);
+    await client.connect();
+    let db = client.db(MONGODB_DB);
+
+    // set cache
+    cachedClient = client;
+    cachedDb = db;
+
+    return {
+        client: cachedClient,
+        db: cachedDb,
+    };
+}
