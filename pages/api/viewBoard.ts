fda93686cd35321128e0835731340c3fe71a5ae5
banma1234 <banma1234@gmail.com>
2023-02-05 00:01:49 +0900

    design: prettier design pattern 적용

--- pages/api/viewBoard.ts
+++ pages/api/viewBoard.ts
@@ -1,27 +1,26 @@
 const { connectToDatabase } = require("util/mongodb");
 const ObjectId = require("mongodb").ObjectId;
 
-export default async function viewBoard(req: any, res: any){
-    try {
-        // connect to the database
-        let { db } = await connectToDatabase();
-        // fetch the posts
-        let posts = await db
-            .collection('posts')
-            .find({})
-            .sort({ uploadDate: -1 })
-            .toArray();
-        // return the posts
-        return res.json({
-            message: JSON.parse(JSON.stringify(posts)),
-            success: true,
-        });
-    } catch (error: any) {
-        // return the error
-        return res.json({
-            message: new Error(error).message,
-            success: false,
-        });
-    }
+export default async function viewBoard(req: any, res: any) {
+  try {
+    // connect to the database
+    let { db } = await connectToDatabase();
+    // fetch the posts
+    let posts = await db
+      .collection("posts")
+      .find({})
+      .sort({ uploadDate: -1 })
+      .toArray();
+    // return the posts
+    return res.json({
+      message: JSON.parse(JSON.stringify(posts)),
+      success: true,
+    });
+  } catch (error: any) {
+    // return the error
+    return res.json({
+      message: new Error(error).message,
+      success: false,
+    });
   }
-  
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,22 +13,29 @@ export default function Home({ posts }: any) {
       <ImgWrapper type="banner">
         <Image src={imgUrl} alt="card Img" priority />
         <OverlapDiv>
-          <Button color="high" ButtonType="default" onClick={()=>{console.log("damn")}}>
+          <Button
+            color="high"
+            ButtonType="default"
+            onClick={() => {
+              console.log("damn");
+            }}
+          >
             Go
           </Button>
         </OverlapDiv>
       </ImgWrapper>
       <section>
         <Board>
-          {posts && posts.map((item:any, i:any) => {
-            return(
-              <Link href={`/posts/${item.title}`} key={i}>
-                <Card type="default" color="low">
-                  <h3>{item.title}</h3>
-                </Card>
-              </Link>
-            );
-          })}
+          {posts &&
+            posts.map((item: any, i: any) => {
+              return (
+                <Link href={`/posts/${item.title}`} key={i}>
+                  <Card type="default" color="low">
+                    <h3>{item.title}</h3>
+                  </Card>
+                </Link>
+              );
+            })}
         </Board>
       </section>
     </Layout>
@@ -38,16 +45,14 @@ export default function Home({ posts }: any) {
 export async function getServerSideProps() {
   let DEV_URL = process.env.DEV_URL;
 
-  // request posts from api
   let response = await fetch(`${DEV_URL ? DEV_URL : ""}/api/viewBoard`, {
-    method: "GET"
+    method: "GET",
   });
-  // extract the data
   let data = await response.json();
 
   return {
-      props: {
-          posts: data['message'],
-      },
+    props: {
+      posts: data["message"],
+    },
   };
 }
