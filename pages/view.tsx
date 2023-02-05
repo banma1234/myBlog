03de74a6c9ebb829e8e3edf1af2383ea509afb0d
banma1234 <banma1234@gmail.com>
2023-02-05 18:44:38 +0900

    test: post 전체보기 및 post.series별로 모아보기 test

---
+++ pages/view.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { CardLayout } from "styles/globals";
+import { Button } from "src/components/atoms";
+import { Card } from "src/components/molecules";
+import { Layout } from "src/components/organisms";
+
+export default function Board() {
+  return (
+    <Layout>
+      <Link href="/view">
+        <Button color="green" ButtonType="small" onClick={null}>
+          Total view
+        </Button>
+      </Link>
+      <Link href="/series">
+        <Button color="gray" ButtonType="small" onClick={null}>
+          Series
+        </Button>
+      </Link>
+    </Layout>
+  );
+}
