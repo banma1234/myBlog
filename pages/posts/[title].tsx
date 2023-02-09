2140439e375cdeb1c3c41c11bc6222bfe903a89a
banma1234 <banma1234@gmail.com>
2023-02-09 18:50:05 +0900

    feat($pages/posts/[title].tsx): 댓글 기능 추가

--- pages/posts/[title].tsx
+++ pages/posts/[title].tsx
@@ -1,7 +1,58 @@
 import Head from "next/head";
 import dynamic from "next/dynamic";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { CommentBox } from 'src/components/organisms';
+import { TextBox, Button, Input } from 'src/components/atoms';
+import { ButtonLayout, FlexEndComponent } from "styles/globals";
+import parseDate from "util/parseDate";
 
 export default function Post({ data }: any) {
+  const [content, setContent] = useState("");
+  const [writter, setWritter] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const initData = () => {
+    setContent("");
+    setError("");
+  }
+
+  const handleComment = async (e: any) => {
+    e.preventDefault();
+
+    setError("");
+    if (!content) return setError("댓글을 입력해주세요");
+
+    let comment = {
+      REF: 3,
+      RE_STEP: 0,
+      RE_LEVEL: 0,
+      date: parseDate(new Date()),
+      writter,
+      content,
+      password,
+      postName: data.post[0].title,
+    }
+
+    let response = await fetch("/api/comments", {
+      method: "POST",
+      body: JSON.stringify(comment),
+    });
+
+    let responseData = await response.json();
+
+    if (responseData.success) {
+      initData();
+      router.replace(router.asPath)
+    } else {
+      alert(responseData.message);
+      return setError(responseData.message);
+    }
+  }
+
   return (
     <>
       <Head>
@@ -9,7 +60,30 @@ export default function Post({ data }: any) {
       </Head>
       <h1>{data.post[0].title}</h1>
       <MarkdownReader style={{ padding: 25 }} source={data.post[0].content} />
-      <button onClick={()=>console.log(data.comment)}>click here!</button>
+      <CommentBox data={data.comment} >
+        <hr/>
+        <ButtonLayout>
+          <Input
+            size="small"
+            placeholder="닉네임"
+            value={writter}
+            type="text"
+            onChange={(e: any) => setWritter(e.target.value)}
+          />
+          <Input
+            size="small"
+            placeholder="비밀번호"
+            value={password}
+            type="password"
+            onChange={(e: any) => setPassword(e.target.value)}
+          />
+        </ButtonLayout>
+        <TextBox placeholder="댓글 입력" value={content} onChange={(e: any) => setContent(e.target.value)} />
+        <FlexEndComponent>
+        <Button color="green" ButtonType="small" onClick={handleComment}>Submit</Button>
+        </FlexEndComponent>
+      </CommentBox>
+      
     </>
   );
 }
