e02abc6d6fb36d82a2e5d2d050b950a8912e9336
banma1234 <banma1234@gmail.com>
2023-02-11 18:54:55 +0900

    feat($hooks/useIcons/~): menu icon 추가

--- util/hooks/useIcons/useIcons.tsx
+++ util/hooks/useIcons/useIcons.tsx
@@ -1,5 +1,5 @@
 /* icon list = {
-    mail, github, arrowUp, enter, like, share, comment, emoji
+    mail, github, arrowUp, enter, like, share, comment, emoji, menu
  } */
 
 export default function useIcons(type: string, size: string) {
@@ -129,6 +129,19 @@ export default function useIcons(type: string, size: string) {
             fill-rule="evenodd"
             d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
           />
+        </svg>
+      );
+    case "menu":
+      return (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={size}
+          height={size}
+          fill="currentColor"
+          className="bi bi-three-dots-vertical"
+          viewBox="0 0 16 16"
+        >
+          <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
         </svg>
       );
   }
