025d079ab8a640a601e7af84e35cc8b6511b8334
banma1234 <banma1234@gmail.com>
2023-03-03 01:20:43 +0900

    design: prittier format 적용

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,30 +1,30 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-    siteUrl: 'https://www.chocoham.dev',
-    changefreq: 'weekly',
-    priority: 0.7,
-    exclude: ['/api/**/*.ts', '/admin.tsx', '/write.tsx'],
-    include: ['./src', './pages'],
-    pagesConfig: [
-      {
-        path: '/posts/[title].tsx',
-        priority: 0.8,
-        exclude: false,
-      },
-      {
-        path: '/series/detail/[series].tsx',
-        priority: 0.5,
-        exclude: false,
-      },
-      {
-        path: '/series/detail/[series].tsx',
-        priority: 0.5,
-        exclude: false,
-      },
-    ],
-    targetFolder: './public',
-    pagesDirectory: __dirname + '/pages',
-    buildId: 'chocoham',
-    additionalSitemaps: ['https://chocoham.dev/new/sitemap.xml'],
-}
+  siteUrl: "https://www.chocoham.dev",
+  changefreq: "weekly",
+  priority: 0.7,
+  exclude: ["/api/**/*.ts", "/admin.tsx", "/write.tsx"],
+  include: ["./src", "./pages"],
+  pagesConfig: [
+    {
+      path: "/posts/[title].tsx",
+      priority: 0.8,
+      exclude: false,
+    },
+    {
+      path: "/series/detail/[series].tsx",
+      priority: 0.5,
+      exclude: false,
+    },
+    {
+      path: "/series/detail/[series].tsx",
+      priority: 0.5,
+      exclude: false,
+    },
+  ],
+  targetFolder: "./public",
+  pagesDirectory: __dirname + "/pages",
+  buildId: "chocoham",
+  additionalSitemaps: ["https://chocoham.dev/new/sitemap.xml"],
+};
