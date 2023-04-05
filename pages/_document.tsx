a6b7fd86e3b10c3a7e9f215eac524c8d55ff4386
banma1234 <banma1234@gmail.com>
2023-04-05 17:13:55 +0900

    feat: naver searchmaster 관련 meta tag 작성

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -37,6 +37,7 @@ class MyDocument extends Document {
       <Html>
         <Head lang="ko">
           <meta name="google-site-verification" content="A1qLguIf9gLBQEkoscbYdxPvOgBxPrI3NF0v3FaFDKU" />
+          <meta name="naver-site-verification" content="cfe40540bf996f7e1a2494dde5497f1616b415e7" />
           <link
             rel="icon"
             type="image/x-icon"
