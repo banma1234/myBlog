fce47b9e3a1f1cbdd3bad3fa32d89d4db60b2813
banma1234 <banma1234@gmail.com>
2023-04-11 14:48:26 +0900

    fix: post url 수정

--- util/accessAdmin.tsx
+++ util/accessAdmin.tsx
@@ -1,5 +1,5 @@
 export default async function accessAdmin(pw: string | null) {
-  let response = await fetch("api/auth/login", {
+  let response = await fetch("/api/auth/login", {
     method: "POST",
     body: pw,
   });
@@ -7,7 +7,9 @@ export default async function accessAdmin(pw: string | null) {
 
   if (data.success) {
     alert(data.message);
+    return true;
   } else {
     alert(data.message);
+    return false;
   }
 }
