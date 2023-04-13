c993d17b118f083cef0127253f43e9e465cced3f
banma1234 <banma1234@gmail.com>
2023-04-13 15:43:52 +0900

    fix: 쿼리속성 이름 변경

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -76,7 +76,7 @@ async function getPosts(req: any, res: any) {
     const options = {
       projection: {
         images: 0,
-        thumbnails: 0,
+        thumbnail: 0,
         isThumbnail: 0,
         series: 0,
       },
