16078767d83de797acca61db557eabdcfd283c4f
banma1234 <banma1234@gmail.com>
2023-04-27 18:36:32 +0900

    design: 모바일 환경에서의 가시성 향상 및 폰트단위 통일

--- src/components/molecules/card/cardStyle.tsx
+++ src/components/molecules/card/cardStyle.tsx
@@ -4,7 +4,6 @@ import { CardType, ObjType } from "./cardType";
 
 export const StyledCard = styled.div.attrs(props => ({}))<CardType>`
   ${props => {
-    const width: ObjType = { default: "90%", login: "400px" };
     const height: ObjType = { default: "300px", login: "700px" };
 
     const temp = props.type;
@@ -30,7 +29,7 @@ export const StyledCard = styled.div.attrs(props => ({}))<CardType>`
       }
       @media all and (max-width: 670px) {
         width: 380px;
-        height: 250px;
+        height: 400px;
       }
       @keyframes cardUp {
         0% {
@@ -63,20 +62,31 @@ export const ImageWrapper = styled.div`
 export const Post = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 0 10px 10px 10px;
 `;
 
 export const TitleBox = styled.div`
-  height: 40px;
+  line-height: 1.2rem;
+  height: 2.4rem;
   font-weight: 700;
-  font-size: 17px;
+  font-size: 1.05rem;
   margin-bottom: 1rem;
-  white-space: nowrap;
+  display: -webkit-box;
+  word-wrap: break-all;
   overflow: hidden;
   text-overflow: ellipsis;
+  -webkit-line-clamp: 2;
+  @media all and (max-width: 670px) {
+    font-size: 1.2rem;
+    height: 2.6rem;
+  }
 `;
 
 export const InfoBox = styled.div`
   display: flex;
   justify-content: flex-end;
+  font-size: 0.85rem;
+  @media all and (max-width: 670px) {
+    font-size: 1rem;
+  }
 `;
