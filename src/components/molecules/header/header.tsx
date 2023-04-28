5a495ce352cdff7174bff7ff72b70a53cc3cd734
banma1234 <banma1234@gmail.com>
2023-04-28 20:17:43 +0900

    feat: 다크모드 state 제어하는 구문 작성

--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -1,18 +1,25 @@
 import {
   StyledHeader,
   HeaderContainer,
-  HeaderIcon,
+  HeaderLogo,
   HeaderMenu,
+  HeaderIcon,
 } from "./headerStyles";
+import { HeaderType } from "./headerType";
 import Link from "next/link";
+import localstorage from "util/localstorage";
+import { useIcons } from "util/hooks";
+import React, { useState, useEffect } from "react";
 
-const HeaderComponent: React.FC = () => {
+const HeaderComponent: React.FC<HeaderType> = (props: HeaderType) => {
+  const darkmode = localstorage("darkmode");
+
   return (
     <StyledHeader>
       <HeaderContainer>
-        <HeaderIcon>
+        <HeaderLogo>
           <Link href="/">Home</Link>
-        </HeaderIcon>
+        </HeaderLogo>
         <HeaderMenu>
           <li>
             <Link href="https://github.com/banma1234">About</Link>
@@ -20,6 +27,11 @@ const HeaderComponent: React.FC = () => {
           <li>
             <Link href="/admin">Login</Link>
           </li>
+          <HeaderIcon onClick={props.onToggle}>
+            {props.theme === "dark"
+              ? useIcons("moon", "22")
+              : useIcons("sun", "22")}
+          </HeaderIcon>
         </HeaderMenu>
       </HeaderContainer>
     </StyledHeader>
