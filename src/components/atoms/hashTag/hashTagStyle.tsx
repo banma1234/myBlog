5d27456dc1bd7957a120472b0175a029013891d2
banma1234 <banma1234@gmail.com>
2023-05-01 02:09:03 +0900

    design: hashtag 위치조정

--- src/components/atoms/hashTag/hashTagStyle.tsx
+++ src/components/atoms/hashTag/hashTagStyle.tsx
@@ -5,10 +5,10 @@ export const HashTagContainer = styled.div`
     display: flex;
     flex-wrap: wrap;
     margin-top: 5rem;
-    gap: 1rem 0.5rem;
+    gap: 0.5rem 1rem;
 `;
 
 export const HashTagBox = styled.div`
   padding: 0.8rem 1.3rem 0.8rem 1.3rem;
-  margin-right: 2rem;
+  margin: 0.5rem;
 `;
