265a299fb1ba30467b1ab43ba72af3d1aa1e17bb
banma1234 <banma1234@gmail.com>
2023-04-30 19:43:17 +0900

    damn

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    // 사용자지정 옵션
+    createDefaultProgram: true,
+    sourceType: "module",
+  },
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+    "@typescript-eslint/parser",
+  ],
+  rules: {
+    // 'React' must be in scope when using JSX 에러 지우기(Next.js)
+    "react/react-in-jsx-scope": "off",
+    // ts파일에서 tsx구문 허용(Next.js)
+    "react/jsx-filename-extension": [1, { extensions: [".ts", ".tsx"] }], // should add ".ts" if typescript project
+  },
+};
---
+++ next-sitemap.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.DEV_URL,
+  generateRobotsTxt: true,
+  changefreq: "weekly",
+  exclude: ["/api/**/*.ts", "/write.tsx, /server-sitemap.xml"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/api/**/*.ts", "/write.tsx'],
+      },
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [`https://chocoham.dev/server-sitemap.xml`],
+  },
+  targetFolder: "./public",
+  pagesDirectory: __dirname + "/pages",
+  buildId: "chocoham",
+  additionalSitemaps: ["https://chocoham.dev/server-sitemap.xml"],
+};
