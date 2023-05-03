4b632ec9e2ad9d8c892581389159633266fba305
banma1234 <banma1234@gmail.com>
2023-05-04 01:42:50 +0900

    fix: merge 충돌 해결

--- pages/api/buildSitemap.ts
+++ pages/api/buildSitemap.ts
@@ -1,40 +1,6 @@
 const { connectToDatabase } = require("util/mongodb");
 
 export default async function buildSitemap(req: any, res: any) {
-<<<<<<< HEAD
-    try{
-      let { db } = await connectToDatabase();
-      const options = {
-        projection: { 
-          id: 0,
-          title: 1,
-          content: 0,
-          series: 0,
-          hashtag: 0,
-          thumbnail: 0,
-          imageTitle: 0,
-          uploadDate: 0,
-        },
-      };
-  
-      let posts = await db
-        .collection("posts")
-        .find({}, options)
-        .toArray();
-  
-      return res.json({
-        message: posts,
-        success: true,
-      });
-    } catch (error: any) {
-      return res.json({
-        message: new Error(error).message,
-        success: false,
-      })
-    }
-  }
-  
-=======
   try {
     let { db } = await connectToDatabase();
     const options = {
@@ -57,4 +23,3 @@ export default async function buildSitemap(req: any, res: any) {
     });
   }
 }
->>>>>>> 440d141f460d143a3b8fdc0b8faeee31b094c983
