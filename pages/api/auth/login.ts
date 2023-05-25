90bd20dbd1a1e7336a53a30a2cefc03cf3fa91f3
banma1234 <banma1234@gmail.com>
2023-05-25 19:02:04 +0900

    feat: session을 이용한 로그인 구현 및 비밀번호 암호화

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,23 +1,73 @@
-export default async function login(req: any, res: any) {
-  try {
-    let result = false;
-    let message = "";
-    if (req.body == process.env.USER_ROOT) {
-      result = true;
-      message = "success";
-    } else {
-      result = false;
-      message = "invalid password error";
-    }
-    return res.json({
-      message: message,
-      success: result,
-    });
-  } catch (error: any) {
-    // return the error
-    return res.json({
-      message: new Error(error).message,
-      success: false,
-    });
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "util/mongodb";
+import { withIronSession, Session } from "next-iron-session";
+import bcrypt from "bcrypt";
+
+declare module "next" {
+  interface NextApiRequest {
+    session: Session;
   }
 }
+
+const sessionConfig = {
+  password: process.env.SESSION_SECRET as string,
+  cookieName: "session",
+  cookieOptions: {
+    secure: process.env.ENVIRONMENT === "production",
+  },
+};
+
+async function loginHandler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  session: Session,
+) {
+  try {
+    const { email, password } = req.body;
+    let { db } = await connectToDatabase();
+
+    const requestUser = await db.collection("user").findOne({ email: email });
+
+    if (requestUser) {
+      const isPasswordMatched = await bcrypt.compare(
+        password,
+        requestUser.password,
+      );
+      if (isPasswordMatched) {
+        session.set("user", { id: requestUser.userName });
+        await session.save();
+
+        return res.json({
+          success: false,
+          message: req.session,
+        });
+      } else {
+        return res.json({
+          success: false,
+          message: "password did not matched",
+        });
+      }
+    } else {
+      return res.json({
+        success: false,
+        message: "user not exist",
+      });
+    }
+  } catch (e: any) {
+    console.log(e);
+  }
+}
+
+export default withIronSession(
+  async (req: NextApiRequest, res: NextApiResponse, session: Session) => {
+    if (req.method === "POST") {
+      await loginHandler(req, res, session);
+    } else {
+      return res.json({
+        success: false,
+        message: "404 not Found",
+      });
+    }
+  },
+  sessionConfig,
+);
