ab89e81b7daac2dae9a90bbc11dfc07049caf6cd
banma1234 <banma1234@gmail.com>
2023-05-26 18:17:09 +0900

    feat: api 사용자 인증 미들웨어 작성

--- util/auth/authMiddleware.ts
+++ util/auth/authMiddleware.ts
@@ -1,47 +1,16 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextApiHandler } from "next";
+import { withIronSession } from "next-iron-session";
 
-export async function withAuth(req: NextRequest) {
-  try {
-    const url = req.nextUrl.clone();
-    url.pathname = "/login";
-
-    const response = await validateUser(req);
-
-    if (response.status === 200) {
-      return NextResponse.next();
-    } else if (response.status === 401) {
-      return NextResponse.redirect(url);
-    }
-  } catch (e: any) {
-    console.log(e);
-  }
-}
-
-export async function withoutAuth(req: NextRequest) {
-  try {
-    const url = req.nextUrl.clone();
-    url.pathname = "/admin";
-
-    const response = await validateUser(req);
-
-    if (response.status === 200) {
-      return NextResponse.redirect(url);
-    } else if (response.status === 401) {
-      return NextResponse.next();
-    }
-  } catch (e: any) {
-    console.log(e);
-  }
-}
-
-function validateUser(req: NextRequest): Promise<Response> {
-  return fetch("/admin", {
-    method: "GET",
-    mode: "cors",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-      cookie: `세션 쿠키 ID = ${req.cookies.get("세션 쿠키 ID")}`,
+const authMiddleware = (handler: NextApiHandler) => {
+  return withIronSession(handler, {
+    password: process.env.SESSION_SECRET as string,
+    cookieName: "session",
+    cookieOptions: {
+      secure: process.env.ENVIRONMENT === "production",
+      httpOnly: process.env.ENVIRONMENT === "production",
+      maxAge: 60 * 60 * 24 * 7,
     },
   });
-}
+};
+
+export default authMiddleware;
